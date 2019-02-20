3e7027d989ed01bd5396c56e1eb6d6a0b06b8251
Zhuyi Xue <alfred532008@gmail.com>
2019-02-20 07:59:34 -0800

    updated kvarscan.py

--- kvarscan.py
+++ kvarscan.py
@@ -20,24 +20,16 @@ logging.basicConfig(level=logging.INFO)
 import ruffus as R
 
 from utils import execute
-
-
-PATH_RE = r'(.*)/(?P<chr>chr[0-9]{1,2}|chr[XYM]|all)/(?P<filetype>\S+)'
+from argparsers.kvarscan import OPTIONS
 
 
 REF_GENOME = os.path.expanduser('~/ref_data/hg19.fa')
 VARSCAN_JAR = os.path.expanduser('~/jars/VarScan.v2.3.7.jar')
+PATH_RE = r'(.*)/(?P<chr>chr[0-9]{1,2}|chr[XYM]|all)/(?P<filetype>\S+)'
 
 
-input_bams = [
-    # '../for_kon_paper/NA19238/reg/chr21/bam/normal.bam'
-    '../for_kon_paper/isim1/reg/chr1/bam/tumour.bam'
-
-    # test set
-    # './set5.dev/all/bam/normal.bam',
-    # './chr21/bam/normal.bam', './chr21/bam/tumour.bam',
-]
-
+input_bams = OPTIONS.bams
+print input_bams
 
 @R.transform(input_bams, R.formatter(PATH_RE),
              ['{path[0]}/{basename[0]}.bam.bai',
@@ -72,8 +64,22 @@ def pileup_gzip(input_bam, outputs):
 
 @R.mkdir(pileup_gzip, R.formatter(PATH_RE), ['{subpath[0][1]}/varscan_mpileup2indel'])
 @R.transform(pileup_gzip, R.formatter(PATH_RE),
-             ['{subpath[0][1]}/varscan_mpileup2indel/{basename[0]}.indel.csv',
+             ['{subpath[0][1]}/varscan_mpileup2indel/{basename[0]}.indel.vcf',
               '{subpath[0][1]}/varscan_mpileup2indel/varscanmpileup2indel.SUCCESS'])
+def varscan_mpileup2indel(inputs, outputs):
+    """by default: append --output-vcf"""
+    pileup_gz, _, _ = inputs
+    output, flag1 = outputs
+    cmd1 = ('java -Xmx12g -jar {VARSCAN_JAR} mpileup2indel '
+            '<(zcat {pileup_gz}) --output-vcf '
+            '> {output}'.format(VARSCAN_JAR=VARSCAN_JAR, **locals()))
+    execute(cmd1, flag=flag1)
+
+
+@R.mkdir(pileup_gzip, R.formatter(PATH_RE), ['{subpath[0][1]}/varscan_mpileup2indel'])
+@R.transform(pileup_gzip, R.formatter(PATH_RE),
+             ['{subpath[0][1]}/varscan_mpileup2indel/{basename[0]}.indel.csv',
+              '{subpath[0][1]}/varscan_mpileup2indel/varscanmpileup2indel.csv.SUCCESS'])
 def varscan_mpileup2indel_csv(inputs, outputs):
     pileup_gz, _, _ = inputs
     output, flag1 = outputs
@@ -81,6 +87,105 @@ def varscan_mpileup2indel_csv(inputs, outputs):
             '<(zcat {pileup_gz}) > {output}'.format(VARSCAN_JAR=VARSCAN_JAR, **locals()))
     execute(cmd1, flag=flag1)
 
+
+@R.transform(pileup_gzip, R.formatter(PATH_RE),
+             ['{subpath[0][1]}/varscan_mpileup2indel/{basename[0]}.pset1.indel.csv',
+              '{subpath[0][1]}/varscan_mpileup2indel/varscanmpileup2indel.pset1.csv.SUCCESS'])
+def varscan_mpileup2indel_pset1_csv(inputs, outputs):
+    pileup_gz, _, _ = inputs
+    output, flag1 = outputs
+    cmd1 = ('java -Xmx12g -jar {VARSCAN_JAR} mpileup2indel '
+            '<(zcat {pileup_gz}) '
+            '--min-var-freq 0 '
+            '>{output}'.format(VARSCAN_JAR=VARSCAN_JAR, **locals()))
+    execute(cmd1, flag=flag1)
+
+
+@R.transform(pileup_gzip, R.formatter(PATH_RE),
+             ['{subpath[0][1]}/varscan_mpileup2indel/{basename[0]}.pset2.indel.csv',
+              '{subpath[0][1]}/varscan_mpileup2indel/varscanmpileup2indel.pset2.csv.SUCCESS'])
+def varscan_mpileup2indel_pset2_csv(inputs, outputs):
+    pileup_gz, _, _ = inputs
+    output, flag1 = outputs
+    cmd1 = ('java -Xmx12g -jar {VARSCAN_JAR} mpileup2indel '
+            '<(zcat {pileup_gz}) '
+            '--min-var-freq 0 '
+            '--p-value 1 '
+            '>{output}'.format(VARSCAN_JAR=VARSCAN_JAR, **locals()))
+    execute(cmd1, flag=flag1)
+
+
+@R.transform(pileup_gzip, R.formatter(PATH_RE),
+             ['{subpath[0][1]}/varscan_mpileup2indel/{basename[0]}.pset3.indel.csv',
+              '{subpath[0][1]}/varscan_mpileup2indel/varscanmpileup2indel.pset3.csv.SUCCESS'])
+def varscan_mpileup2indel_pset3_csv(inputs, outputs):
+    pileup_gz, _, _ = inputs
+    output, flag1 = outputs
+    cmd1 = ('java -Xmx12g -jar {VARSCAN_JAR} mpileup2indel '
+            '<(zcat {pileup_gz}) '
+            '--min-var-freq 0 '
+            '--p-value 1 '
+            '--strand-filter 0 '
+            '>{output}'.format(VARSCAN_JAR=VARSCAN_JAR, **locals()))
+    execute(cmd1, flag=flag1)
+
+
+@R.transform(pileup_gzip, R.formatter(PATH_RE),
+             ['{subpath[0][1]}/varscan_mpileup2indel/{basename[0]}.pset4.indel.csv',
+              '{subpath[0][1]}/varscan_mpileup2indel/varscanmpileup2indel.pset4.csv.SUCCESS'])
+def varscan_mpileup2indel_pset4_csv(inputs, outputs):
+    pileup_gz, _, _ = inputs
+    output, flag1 = outputs
+    cmd1 = ('java -Xmx12g -jar {VARSCAN_JAR} mpileup2indel '
+            '<(zcat {pileup_gz}) '
+            '--min-var-freq 0 '
+            '--p-value 1 '
+            '--strand-filter 0 ' # default 1
+            '--min-reads2 1 '    # default 2
+            '>{output}'.format(VARSCAN_JAR=VARSCAN_JAR, **locals()))
+    execute(cmd1, flag=flag1)
+
+
+
+@R.mkdir(pileup_gzip, R.formatter(PATH_RE), ['{subpath[0][1]}/varscan_somatic'])
+@R.collate(pileup_gzip, R.formatter(PATH_RE),
+           ['{subpath[0][1]}/varscan_somatic/pset4.snp.vcf.gz',
+            '{subpath[0][1]}/varscan_somatic/pset4.snp.vcf.gz.tbi',
+            '{subpath[0][1]}/varscan_somatic/pset4.indel.vcf.gz',
+            '{subpath[0][1]}/varscan_somatic/pset4.indel.vcf.gz.tbi',
+            '{subpath[0][1]}/varscan_somatic/varscan_somatic_pset4.SUCCESS',
+            '{subpath[0][1]}/varscan_somatic/varscan_somatic_pset4_bgzip_tabix_snp.SUCCESS',
+            '{subpath[0][1]}/varscan_somatic/varscan_somatic_pset4_bgzip_tabix_indel.SUCCESS'])
+def varscan_somatic_pset4(inputs, outputs):
+    inputs = [_ for _ in itertools.chain(*inputs)
+              if not _.endswith('.SUCCESS')]
+    snp_vcf, snp_tbi, indel_vcf, indel_tbi, flag1, flag2, flag3 = outputs
+    output_prefix = snp_vcf.replace('.snp.vcf.gz', '')
+    # how to preserve the order of normal, tumour?, assert is just a dirty
+    # quick hack
+    assert len(inputs) == 2
+    assert 'normal' in inputs[0]
+    assert 'tumour' in inputs[1]
+    cmd1 = ('java -Xmx12g -jar {VARSCAN_JAR} somatic '
+            '<(zcat {normal_pileup_gz}) '
+            '<(zcat {tumour_pileup_gz}) '
+            '{output_prefix} '
+            '--min-var-freq 0 '
+            '--p-value 1 '
+            '--strand-filter 0 ' # default 1
+            '--min-reads2 1 '    # default 2
+            '--somatic-p-value 1 '
+            '--output-vcf'.format(VARSCAN_JAR=VARSCAN_JAR,
+                                  normal_pileup_gz=inputs[0],
+                                  tumour_pileup_gz=inputs[1],
+                                  output_prefix=output_prefix))
+    execute(cmd1, flag=flag1)
+    cmd2 = 'bgzip -f {0} && tabix -p vcf {1}'.format(
+        snp_vcf.replace('.gz', ''), snp_vcf)
+    execute(cmd2, flag=flag2)
+    cmd3 = 'bgzip -f {0} && tabix -p vcf {1}'.format(
+        indel_vcf.replace('.gz', ''), indel_vcf)
+    execute(cmd3, flag=flag3)
 
 
 
@@ -133,16 +238,11 @@ if __name__ == "__main__":
     print os.environ['PWD']
     print '=' * 79
 
-    parser = R.cmdline.get_argparse(
-        description="krvarscan",
-        usage='require python-2.7.x',
-        version='0.1')
-    options = parser.parse_args()
     logger, logger_mutex = R.cmdline.setup_logging(
-        __name__, options.log_file, options.verbose)
+        __name__, OPTIONS.log_file, OPTIONS.verbose)
 
-    logger.info(options.verbose)
+    logger.info(OPTIONS.verbose)
     # with logger_mutex:
     #     logger.info("Look Ma. No hands")
     R.pipeline_printout_graph('lele.svg', draw_vertically=True)
-    R.cmdline.run(options)
+    R.cmdline.run(OPTIONS)
